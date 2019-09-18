32b91ed758ea17d97d512be9ab252ba45fbdc8ce
Cristian Deluxe <djcristiandeluxe@gmail.com>
2019-09-18 20:17:27 +0200

    Moved Shared Variables to Blade Service Provider

--- app/Providers/BladeServiceProvider.php
+++ app/Providers/BladeServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Organiser;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use JavaScript;
 
 class BladeServiceProvider extends ServiceProvider
 {
@@ -27,5 +30,24 @@ class BladeServiceProvider extends ServiceProvider
         Blade::directive('money', function ($expression) {
             return "<?php echo number_format($expression, 2); ?>";
         });
+
+        /*
+         * Share the organizers across all views
+         */
+        View::share('organisers', Organiser::scope()->get());
+
+        /*
+         * Set up JS across all views
+         */
+        JavaScript::put([
+            'User'                => [
+                'full_name'    => Auth::user()->full_name,
+                'email'        => Auth::user()->email,
+                'is_confirmed' => Auth::user()->is_confirmed,
+            ],
+            'DateTimeFormat'      => config('attendize.default_date_picker_format'),
+            'DateSeparator'       => config('attendize.default_date_picker_seperator'),
+            'GenericErrorMessage' => trans("Controllers.whoops"),
+        ]);
     }
 }
