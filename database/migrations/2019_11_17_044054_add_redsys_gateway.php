de3ac499a59cbe2071af72bfba71c325e6f315c7
Cristian Deluxe <djcristiandeluxe@gmail.com>
2019-11-17 05:44:08 +0100

    Create Redsys migration

---
+++ database/migrations/2019_11_17_044054_add_redsys_gateway.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddRedsysGateway extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('payment_gateways')->insert(
+            [
+                'provider_name'           => 'Redsys',
+                'provider_url'            => 'http://www.redsys.es/',
+                'is_on_site'              => 0,
+                'can_refund'              => 1,
+                'name'                    => 'Redsys',
+                'default'                 => 0,
+                'admin_blade_template'    => 'ManageAccount.Partials.Redsys',
+                'checkout_blade_template' => 'Public.ViewEvent.Partials.PaymentRedsys'
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('payment_gateways')->where('name', '=', 'Redsys')->delete();
+    }
+}
