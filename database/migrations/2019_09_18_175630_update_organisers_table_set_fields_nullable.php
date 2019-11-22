08d6b82605355b88dc95ec0ced226f29f5973780
Cristian Deluxe <djcristiandeluxe@gmail.com>
2019-11-22 03:07:57 +0100

    Set a default value when updating fields as non nullable

--- database/migrations/2019_09_18_175630_update_organisers_table_set_fields_nullable.php
+++ database/migrations/2019_09_18_175630_update_organisers_table_set_fields_nullable.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class UpdateOrganisersTableSetFieldsNullable extends Migration
 {
@@ -31,12 +31,12 @@ class UpdateOrganisersTableSetFieldsNullable extends Migration
     public function down()
     {
         Schema::table('organisers', function (Blueprint $table) {
-            $table->text('about')->nullable(false)->change();
-            $table->string('tax_id')->nullable(false)->change();
-            $table->string('tax_name')->nullable(false)->change();
-            $table->string('tax_value')->nullable(false)->change();
-            $table->string('facebook')->nullable(false)->change();
-            $table->string('twitter')->nullable(false)->change();
+            $table->text('about')->nullable(false)->default('')->change();
+            $table->string('tax_id')->nullable(false)->default('')->change();
+            $table->string('tax_name')->nullable(false)->default('')->change();
+            $table->string('tax_value')->nullable(false)->default('')->change();
+            $table->string('facebook')->nullable(false)->default('')->change();
+            $table->string('twitter')->nullable(false)->default('')->change();
         });
     }
 }
