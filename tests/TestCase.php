2ce8b1fcd6da6125d46d7767e638ff40eb50a3aa
Cristian Deluxe <djcristiandeluxe@gmail.com>
2019-11-22 03:34:34 +0100

    Restore Test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,22 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Tests\Concerns\DatabaseSetup;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseSetup;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->setupDatabase();
+    }
+
+    public function assertDatabaseHasMany(array $expected = [])
+    {
+        collect($expected)->each(function ($data, $table) {
+            $this->assertDatabaseHas($table, $data);
+        });
+    }
 }
