e2887fdc848ddf2068d5690fa4c18acb3f455185
Cristian Deluxe <djcristiandeluxe@gmail.com>
2019-11-22 19:23:57 +0100

    Added documentation

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 
+/**
+ * @property bool is_cancelled
+ * @property Order order
+ * @property string first_name
+ * @property string last_name
+ */
 class Attendee extends MyBaseModel
 {
     use SoftDeletes;
@@ -27,7 +33,7 @@ class Attendee extends MyBaseModel
     ];
 
     protected $casts = [
-        'is_refunded' => 'boolean',
+        'is_refunded'  => 'boolean',
         'is_cancelled' => 'boolean',
     ];
 
@@ -53,7 +59,7 @@ class Attendee extends MyBaseModel
     }
 
     /**
-     * @param array $attendeeIds
+     * @param  array  $attendeeIds
      * @return Collection
      */
     public static function findFromSelection(array $attendeeIds = [])
